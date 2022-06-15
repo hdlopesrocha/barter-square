06ee5d39f71b9c53706b0eff9e38b846691637ae
hdlrocha <henrique.rocha@cern.ch>
2022-06-15 14:30:07 +0200

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Profile from "./pages/Profile";
+import NoPage from "./pages/NoPage";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
